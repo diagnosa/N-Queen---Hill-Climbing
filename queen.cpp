25c17780fefadccdfca83cb27ad2f11e3cbd0f12
natsumiaya <natsumiaya@live.com>
2014-10-30 21:22:21 +0700

    Update queen.cpp
    gethvalue belum fix

--- queen.cpp
+++ queen.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 vector<int> kolom;
 vector< vector<int> > papan;
-
+vector< vector<int> > hvalue;
 void inisialisasi(int n){
 	int i, j, b;
 	for(i=0;i<n;i++){
@@ -22,6 +22,7 @@ void inisialisasi(int n){
 		b= rand()%n;
 		kolom[b]=1;
 		papan.push_back(kolom);
+		hvalue.push_back(kolom);
 	}
 }
 void cetak(int n){
@@ -34,11 +35,48 @@ void cetak(int n){
 	}
 }
 
+void gethcost (int len){
+	int h =0, offset;
+	for (int k=0; k<len; k++)
+	{
+		for (int i=0, j=i+1; j < len; i++)
+		{
+				offset = j-i;
+				if ((papan[k][i]==papan[k][j])&&(papan[k][i]==1)&&(papan[k][j]==1)) 
+				{
+					h+=1;
+					hvalue[k][i]=h;
+					hvalue[k][j]=h;
+				}
+				if ((papan[k][i]==papan[k][j-offset])&&(papan[k][i]==1) || (papan[k][i]==papan[k][j+offset])&&(papan[k][i]==1))
+				{
+					h+=1;
+					hvalue[k][i]=h;
+					hvalue[k][j]=h;
+				}
+				j++;
+		}
+	}
+	
+}
+
+void cetakh(int n){
+	int i, j;
+	for(i=0; i<n; i++){
+		for(j=0;j<n;j++){
+			cout << hvalue[j][i] << " ";
+		}
+		cout << endl;
+	}
+}
 int main(){
 	int n, j , i;
 	cout << "isi banyak queen : ";
 	scanf("%d", &n);
 	inisialisasi(n);
 	cetak(n);
+	gethcost(n);
+	printf ("cek\n");
+	cetakh(n);
 	return 0;
 }
